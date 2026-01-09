13bc73f19b61aa793b4b96e65ae60529344b305d
WaithakaGuru <waithakaoffices@gmail.com>
2026-01-09 09:02:51 +0300

    setup: configure fontawesome icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
-
-
   return (
     <>
-      <div className='text-amber-700'>Mutsda</div>
+      <div className='text-amber-700'>
+        Mutsda <FontAwesomeIcon icon="coffee" size="7x"/>
+      </div>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// font awesome configs
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
+
+// import the icons file to avoid reimporting 
+import "./icons.js"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
