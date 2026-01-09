2ce31e12a7946fb5c4224c16f5745d6501634495
WaithakaGuru <waithakaoffices@gmail.com>
2026-01-09 09:57:38 +0300

    Setup: backend server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+// setup a simple express server 
+import {Hono} from 'hono';
+const App = new Hono()
+const handleDefaultRoute = (c) => c.html(`
+    <html>
+        <body>
+        MUTSDA SERVER
+        </body>
+        <input type=file name=file/>
+    </html>    
+`)
+
+App.get("/", handleDefaultRoute)
+
+export default App
+
+
