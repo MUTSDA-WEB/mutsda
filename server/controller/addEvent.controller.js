b7e6f5972a70b05224eeb6bdbdbc9dbe1420b5b9
WaithakaGuru <waithakaoffices@gmail.com>
2026-01-22 15:42:27 +0300

    feat: add 'createEvent' controller

---
+++ server/controller/addEvent.controller.js
@@ -0,0 +1,31 @@
+import App from "..";
+import client from "../helpers/prismaClient";
+
+export default async function (c) {
+   const eventInfo = c.req.json();
+   const { title, descritption, imageURL, eventStartDate, eventEndDate } =
+      eventInfo;
+
+   // saving data to db
+   try {
+      const newEvent = await client.event.create({
+         data: {
+            title,
+            description,
+            imageURL,
+            eventEndDate,
+            eventStartDate,
+         },
+      });
+
+      if (newEvent) {
+         return c.json(
+            { message: "Event created Successfully", event: newEvent },
+            201,
+         );
+      }
+   } catch (error) {
+      console.log(error);
+      return c.json({ error: "Failed to Create new event!!" }, 500);
+   }
+}
