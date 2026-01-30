8c378d5a149c7af240eae813f2356aacd56b9921
SAMKIZ990 <mwangisammy20430@gmail.com>
2026-01-30 14:48:22 +0300

    update axios helper and footer layout

--- mutsda/client/src/helpers/axios.js
+++ mutsda/client/src/helpers/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE = import.meta.env.VITE_API_URL || "http://localhost:4000";
+
 const Ax = axios.create({
-   baseURL: "http://localhost:4554",
+   baseURL: BASE,
    withCredentials: true,
 });
 
